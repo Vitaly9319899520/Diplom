0e1b03032f2f66b9b4969e764d2316e499088578
Maslykou Vitalii <9319899520@mail.ru>
2025-10-05 17:58:00 +0300

    comm-1 - add gitignore

--- main/views.py
+++ main/views.py
@@ -2,11 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    # context: {
-    #     'title':'Home',
-    #     'content':'Главная страница магазина'
-    # }
-
     return render(request, 'main/index.html', context={'title':'Home','content':'Главная страница' })
 
 def about(request):
