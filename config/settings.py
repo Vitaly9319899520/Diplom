a26ad8b69dee9a8f964dc45852a6c2ce33cc2de5
Maslykou Vitalii <9319899520@mail.ru>
2025-10-09 20:57:36 +0300

    comm-1 - add register

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os.path
 from pathlib import Path
 
-from django.conf.global_settings import STATICFILES_DIRS
+from django.conf.global_settings import STATICFILES_DIRS, MEDIA_URL, MEDIA_ROOT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,6 +136,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 INTERNAL_IPS = [
     # ...
--- main/views.py
+++ main/views.py
@@ -2,10 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from goods.models import Categories
 
-def index(request):
-
-    categories = Categories.objects.all()
-    return render(request, 'main/index.html', context={'title':'Home-Главная','content':'Магазин мебели HOME', 'categories': categories })
+def index(request):\
+    return render(request, 'main/index.html', context={'title':'Home-Главная','content':'Магазин мебели HOME'})
 
 def about(request):
     return render(request, 'main/about.html', context={'title':'Home-О нас','content':'О нас', 'text_on_page':'Текст о магазине'})
