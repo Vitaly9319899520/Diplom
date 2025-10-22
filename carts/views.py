aeb3ae3456126525e150ab90f4844c400b706ca4
Maslykou Vitalii <9319899520@mail.ru>
2025-10-22 14:22:35 +0300

    comm-2 - docker-20-final

--- carts/views.py
+++ carts/views.py
@@ -16,6 +16,7 @@ def cart_add(request, product_slug):
             Cart.objects.create(user=request.user, product=product, quantity=1)
     return redirect(request.META["HTTP_REFERER"])
 
+
 def cart_change(request, cart_id):
     pass
 
